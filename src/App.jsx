d374e19a927aa9eba6db6785ee5266ee2e310c15
Edi Edwardson C. Meer <edison.cmeer@gmail.com>
2024-06-03 23:15:52 +0800

    Added edit function

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import JobPage, {jobLoader} from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
@@ -32,6 +33,18 @@ const App = () => {
     return;
   };
 
+  // Update Job
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job)
+    });
+    return;
+  };
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={ <MainLayout /> }>
@@ -39,6 +52,7 @@ const App = () => {
         <Route path="/jobs" element={ <JobsPage/> }/>
         <Route path="/add-job" element={ <AddJobPage addJobSubmit={addJob}/> }/>
         <Route path="/jobs/:id" element={ <JobPage deleteJob={deleteJob} /> } loader={jobLoader}/>
+        <Route path="/edit-job/:id" element={ <EditJobPage updateJobSubmit={updateJob} /> } loader={jobLoader}/>
         <Route path="*" element={ <NotFoundPage/> }/>
       </Route>
     )
